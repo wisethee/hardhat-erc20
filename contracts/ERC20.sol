48491eb00b23f69774e493d178b55ac572f1ab2c
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-10-06 11:00:25 +0100

    feat: add transfer, and transferFrom functions to deal with the token transfer and add the approve function to validate the transaction

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,6 +6,9 @@ contract ERC20 {
     string public name;
     string public symbol;
 
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
     constructor(string memory _name, string memory _symbol) {
         name = _name;
         symbol = _symbol;
@@ -13,5 +16,55 @@ contract ERC20 {
 
     function decimals() external pure returns (uint8) {
         return 18;
+    }
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool)
+    {
+        return _transfer(msg.sender, recipient, amount);
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        uint256 currentAllowance = allowance[sender][msg.sender];
+        require(
+            currentAllowance >= amount,
+            "ERC20: transfer amount exceeds allowance"
+        );
+
+        allowance[sender][msg.sender] = currentAllowance - amount;
+
+        return _transfer(sender, recipient, amount);
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "ERC20: transfer to the zero address");
+
+        allowance[msg.sender][spender] = amount;
+
+        return true;
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) private returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 senderBalance = balanceOf[sender];
+        require(
+            senderBalance >= amount,
+            "ERC20: transfer amount exceeds balance"
+        );
+
+        balanceOf[sender] = senderBalance - amount;
+        balanceOf[recipient] += amount;
+
+        return true;
     }
 }
